30421619822515c30c3437cc06174de70d8a3cbc
JeasonKim <jeason.kim.cn@gmail.com>
2024-09-30 15:36:21 +0800

    1、修改启动ip 2、修改pexels调用的ua避免403的错误

--- app/services/material.py
+++ app/services/material.py
@@ -40,7 +40,11 @@ def search_videos_pexels(
     video_orientation = aspect.name
     video_width, video_height = aspect.to_resolution()
     api_key = get_api_key("pexels_api_keys")
-    headers = {"Authorization": api_key}
+    headers = {
+        "Authorization": api_key,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/58.0.3029.110 Safari/537.3"
+    }
     # Build URL
     params = {"query": search_term, "per_page": 20, "orientation": video_orientation}
     query_url = f"https://api.pexels.com/videos/search?{urlencode(params)}"
@@ -162,7 +166,12 @@ def save_video(video_url: str, save_dir: str = "") -> str:
     with open(video_path, "wb") as f:
         f.write(
             requests.get(
-                video_url, proxies=config.proxy, verify=False, timeout=(60, 240)
+                video_url,
+                headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                                       "like Gecko) Chrome/58.0.3029.110 Safari/537.3"},
+                proxies=config.proxy,
+                verify=False,
+                timeout=(60, 240)
             ).content
         )
 
